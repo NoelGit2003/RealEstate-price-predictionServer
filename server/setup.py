b0004306cb2f9d85499e4c3559c8f197d59bf5e8
NoelGit2003 <noelnmwilliam@gmail.com>
2024-05-04 11:40:20 +0530

    build

--- server/setup.py
+++ server/setup.py
@@ -7,10 +7,7 @@ setup(
     install_requires=[     
         'flask',
         'python-dotenv',
-        'json', 
-        'pickle',
         'numpy',
-        'flask_cors',
-        'os'
+        'flask-cors'
     ],
 )
