198e79d4c3afc6ab5fa810aabbd751215d408047
NoelGit2003 <noelnmwilliam@gmail.com>
2024-05-04 10:16:29 +0530

    updates

--- server/server.py
+++ server/server.py
@@ -2,7 +2,10 @@ from flask import Flask, request, jsonify
 import util
 from util import get_estimated_price
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -16,19 +19,6 @@ def get_location_names():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-
-# @app.route('/predict_home_price', methods = ['GET', 'POST'])
-# def predict_home_price():
-#     total_sqft = float(request.form['total_sqft'])
-#     location = request.form['location']
-#     bhk = int(request.form['bhk'])
-#     bath = int(request.form['bath'])
-    
-#     response = jsonify({
-#         'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
-#     })
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
 
 @app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
@@ -49,13 +39,17 @@ def predict_home_price():
     except Exception as e:
         error_message = str(e) if str(e) else "An error occurred while processing the request"
         return jsonify({'error': error_message}), 400
-    
+ 
+ 
+port = os.getenv("PORT")   
     
 if __name__ == "__main__":
     print("Starting server for AI prediction")
     util.load_saved_artifacts()
     print(util.get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
-    app.run()
+    app.run(port=int(port))
+    
+
     
 
     
